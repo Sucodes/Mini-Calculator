fb4c324f6b304ad53250b4f803d8b88335e8b232
Money Oghenesuvwe <rhounamoney@gmail.com>
2022-07-09 18:32:58 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Mini Calculator Project
+
+![Calculator project banner](./images/Mini_Calculator.png)
+---
+
+## Basic Overview
+
+This is a Simple Calculator project made to perform basic arithmetic functions. It was built entirely with
+
+- HTML
+- SASS
+- JAVASCRIPT
+
+---
+
+## Show tour support
+
+Give a &#x2605; if you like this project!
---
+++ index.js
@@ -0,0 +1,35 @@
+//selectors
+const td = document.querySelectorAll("td");
+const displayScreen = document.getElementById("display");
+const result = document.getElementById("result");
+
+//eventlistener
+td.forEach(td=> {
+    td.addEventListener("click", calculate);
+});
+
+// function to evaluate numbers
+function calculate() {
+    let number = this.innerText;
+
+    if (number === "C") {
+        displayScreen.innerText = "";
+        result.innerText = "";
+        return;
+    }
+
+    else if (number === "&#8634;") {
+        displayScreen.innerText = displayScreen.textContent.substring(0, displayScreen.textContent.length-1);
+        return;
+    }
+
+    else if (number === "=") {
+        result.innerText = eval(displayScreen.innerText);
+        return;
+    }
+
+    else {
+        displayScreen.textContent += number;
+        return; 
+    };
+};
